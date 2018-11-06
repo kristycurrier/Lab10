d804fa98ee386182831a2dd07849602871f26523
Kristy Currier <kristy.currier@gmail.com>
2018-11-06 14:18:40 -0500

    Calculations in circle class complete

---
+++ Lab10/Circle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+        private double _radius;
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; set; }
+
+        public void SetRadius(double newRadius)
+        {
+            _radius = newRadius;
+        }
+
+        public double GetRadius()
+        {
+            return _radius;
+        }
+
+        public double CalculateCircumference(double radius)
+        {
+            double diameter = radius * 2;
+            double circumference = Math.PI * diameter;
+            return circumference;
+        }
+
+        public string CalculateFormattedCircumference(double radius)
+        {
+            double diameter = radius * 2;
+            double circumference = Math.PI * diameter;
+            double formattedCircumference = Math.Round(circumference, 2);
+            string formattedCircumferenceString = Convert.ToString(formattedCircumference);
+            return formattedCircumferenceString;
+        }
+
+        public double CalculateArea(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+        
+        public string CalculateFormattedArea(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            double formattedArea = Math.Round(area, 2);
+            string formattedAreaString = Convert.ToString(formattedArea);
+            return formattedAreaString;
+        }
+
+        private double FormatNumber(double x)
+        {
+            double formattedNumber = Math.Round(x, 2);
+            return formattedNumber;
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester");
+            bool realDouble = false;
+            double radius=-1;
+            while (realDouble == false || radius <= 0)
+            {
+                Console.Write("Enter radius: ");
+                string input = Console.ReadLine();
+                realDouble = double.TryParse(input, out radius);
+
+                if (radius <= 0)
+                {
+                    Console.WriteLine("Sorry, that is not a valid radius.  It must a number greater then zero.");
+                }
+            }
+            var newCircle = new Circle(radius);
+
+            Console.WriteLine(newCircle.Radius);
+            string formmatedCircumfernece =  newCircle.CalculateFormattedCircumference(newCircle.Radius);
+            Console.WriteLine($"Circumference: {formmatedCircumfernece}");
+            string formmatedArea = newCircle.CalculateFormattedArea(newCircle.Radius);
+            Console.WriteLine($"Area: {formmatedArea}");
+            Console.ReadKey();
+
+        }
+    }
+}
